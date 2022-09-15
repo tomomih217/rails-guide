0049c1d0e26a55b7d93297bf3a7df16842e8a9c5
tomomih217 <ku2gelschrei21ber7@gmail.com>
2022-09-15 13:03:33 +0900

    add initialize

--- chapter1.rb
+++ chapter1.rb
@@ -1,7 +1,14 @@
 class User
-    attr_accessor :name, :address, :email
+    attr_reader :name, :address, :email
+
+    def initialize(name:, address:, email:)
+        @name = name
+        @address = name
+        @email = email
+    end
 
     def introduce
         "#{@name}(#{address})"
     end
+    
 end
